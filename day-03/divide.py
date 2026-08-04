70be32c9725c2693e01b93abc0795400974f3965
varshitha <varshithahm08@gmail.com>
2026-08-04 20:55:51 +0530

    Completed Day 3 - User Input Practice

---
+++ day-03/divide.py
@@ -0,0 +1,4 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Division =", num1 / num2)
---
+++ day-03/stddet.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+course = input("Enter your course: ")
+
+print("\n----- Student Details -----")
+print("Name :", name)
+print("Age :", age)
+print("Course :", course)
