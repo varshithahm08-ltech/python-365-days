bf997960d607017a0284612a7fa9b4ab87e92b36
varshitha <varshithahm08@gmail.com>
2026-08-06 20:20:18 +0530

    Merge local project with GitHub

--- day-03/project3.py
+++
@@ -1,8 +0,0 @@
-name = input("Enter Student Name: ")
-age = int(input("Enter Age: "))
-course = input("Enter Course: ")
-
-print("\n===== STUDENT PROFILE =====")
-print("Name   :", name)
-print("Age    :", age)
-print("Course :", course)
--- day-05/project day5.py
+++
@@ -1,8 +0,0 @@
-secret = 7
-
-guess = int(input("Guess the Number (1-10): "))
-
-if guess == secret:
-    print("Congratulations! You guessed correctly.")
-else:
-    print("Wrong guess. Try Again.")
