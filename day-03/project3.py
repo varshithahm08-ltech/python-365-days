d7ec4e2b2161d55c5439c4eb3a54944c50b7a9c2
varshitha <varshithahm08@gmail.com>
2026-08-06 19:55:36 +0530

    Day 6: Python Loops and Number Guessing Game

---
+++ day-03/project3.py
@@ -0,0 +1,8 @@
+name = input("Enter Student Name: ")
+age = int(input("Enter Age: "))
+course = input("Enter Course: ")
+
+print("\n===== STUDENT PROFILE =====")
+print("Name   :", name)
+print("Age    :", age)
+print("Course :", course)
