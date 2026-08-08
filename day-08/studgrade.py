fe075da3e8661f0e886cddaa21ab51a90b62daf9
varshitha <varshithahm08@gmail.com>
2026-08-08 20:37:56 +0530

    Day 8 Python conditional statements

---
+++ day-08/studgrade.py
@@ -0,0 +1,12 @@
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Grade A+")
+elif marks >= 75:
+    print("Grade A")
+elif marks >= 60:
+    print("Grade B")
+elif marks >= 35:
+    print("Grade C")
+else:
+    print("Fail")
